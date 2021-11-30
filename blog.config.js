45f6447ca91c73d585dece8de450723bb27f93a6
hpp0hpp <54829297+hpp0hpp@users.noreply.github.com>
2021-11-30 22:39:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -32,7 +32,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-R5S1GCZ2L0' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
